619300efa3621cf6f5b93a1f2347fd12938e1725
Tabije-10ruby-ay2627 <tabije.ae@obmontessori.edu.ph>
2026-09-20 20:56:07 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+from pyscript import display, document
+
+def create_order(e):
+        document.getElementById("result").innerHTML = " "
+        prod1 = document.getElementById("item1-label")
+        prod2 = document.getElementById("item2-label")
+        prod3 = document.getElementById("item3-label")
+
+        subtotal = float(prod1.value) * prod1.checked + float(prod2.value) * prod2.checked + float(prod3.value) * prod3.checked
+        display(f'The subtotal is {subtotal}', target="result")
+
+        vat_number = subtotal * 0.12
+        display(f'The VAT is {vat_number}', target="result")
+
+        total_amount = subtotal + vat_number
+        display(f'The total amount is {total_amount}', target="result")
+
+# Creating a SKU Generator
+
+def generate_sku(e):
+
+    category = document.getElementById("category")
+    product = document.getElementById("product")
+    stock = document.getElementById("stock")
+
+    category_code = category.value
+    product_name = product.value
+    stock_quantity = stock.value
+
+    product_code = product_name.upper()
+    product_code = product_code.replace(" ", "")
+    product_code = product_code[:4]
+
+    sku = category_code + "-" + product_code + "-" + stock_quantity
+
+    document.getElementById("result").textContent = sku
